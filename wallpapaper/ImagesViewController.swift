05470d14900b5936a0a2999247c2aca7e54a0d27
Abhilash Knox George <user.abhilash@gmail.com>
2022-11-30 05:00:57 +0530

    Final Commit - Complete project

--- wallpapaper/ImagesViewController.swift
+++ wallpapaper/ImagesViewController.swift
@@ -13,22 +13,83 @@ class ImagesViewController: UIViewController {
     @IBOutlet weak var creditLabel: UILabel!
     
     var category = ""
-    
+    var imageCounter = 0
     var appID = "dtKKqC4Cnqk_7YZZQeV4lvlMoPqXXET2wP5aJAfjlkM"
     var images = [JSON]()
     var imageView = [UIImageView]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        imageView = view.subviews.compactMap{$0 as? UIImageView}
+        imageView.forEach {$0.alpha = 0}
+        creditLabel.layer.cornerRadius = 15
+        
+        guard let url = URL(string: "https://api.unsplash.com/search/photos?client_id=\(appID)&query=\(category)&per_page=100") else {return}
+        
+        DispatchQueue.global(qos: .userInteractive).async {
+            self.fetch(url)
+        }
     }
     
     func fetch(_ url: URL) {
         
+        if let data = try? Data(contentsOf: url){
+            let json = JSON(data)
+            images = json["results"].arrayValue
+            downloadImage()
+        }
+        
     }
     
     func downloadImage() {
+        let currentImage = images[imageCounter % images.count]
         
+        let imageName = currentImage["urls"]["full"].stringValue
+        let imageCredit = currentImage["user"]["name"].stringValue
+        
+        imageCounter += 1
+        
+        guard let imageURL = URL(string: imageName) else {return}
+        guard let imageData = try? Data(contentsOf: imageURL) else {return}
+        
+        guard let image = UIImage(data: imageData) else {return}
+        
+        
+        DispatchQueue.main.async {
+            self.show(image, credit: imageCredit)
+        }
+           
+    }
+    
+    func show(_ image: UIImage, credit: String) {
+        spinner.stopAnimating()
+        
+        let imageViewToUse = imageView[imageCounter % imageView.count]
+        let otherImageView = imageView[(imageCounter+1) % imageView.count]
+        
+        UIView.animate(withDuration: 2.0, animations: {
+            imageViewToUse.image = image
+            imageViewToUse.alpha = 1
+            
+            self.creditLabel.alpha = 0
+            
+            self.view.sendSubviewToBack(otherImageView)
+        }) { _ in
+            self.creditLabel.text = ("   \(credit.uppercased())")
+            self.creditLabel.alpha = 1
+            otherImageView.alpha = 0
+            otherImageView.transform = .identity
+            
+            UIView.animate(withDuration: 10.0, animations: {
+                imageViewToUse.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+            }) { _ in
+                DispatchQueue.global(qos: .userInteractive).async {
+                    self.downloadImage()
+                }
+            }
+        }
     }
 
 
